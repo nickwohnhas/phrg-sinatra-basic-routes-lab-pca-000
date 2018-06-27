0e90e87a599f4e859498829ac2b7f1f7fba4392c
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-06-27 10:48:51 -0400

    Built basic routes with Sinatra

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/name' do
+  "My name is Nick"
+end
+
+get '/hometown' do
+  "My hometown is Germantown, MD"
+end
+
+get '/favorite-song' do
+  'My favorite song is Achilles Last Stand'
+end
+
 end
